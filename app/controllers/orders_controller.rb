e47763e4f365000e8b841b073ddb0b7e80f9aad2
GK4Zj <107035536+GK4Zj@users.noreply.github.com>
2024-06-24 21:04:07 +0300

    Add files via upload

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,15 @@
+Class OrdersController < ApplicationController
+  before_filter :authenticate_user!, except => [:show, :index]
+  
+  def index
+  
+  end
+  
+  def show
+  
+  end
+  
+  def create
+  
+  end
+end
